80b6add73d69348b94f5ccf2696b8b15ea5934f7
guedes674 <a97369@alunos.uminho.pt>
2024-11-01 12:48:47 +0000

    main com os paths certos e com os frees

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/parser.h"
+#include "../include/queries.h"
 
 int main(){
     GHashTable* artist_table = createArtistTable();
     GHashTable* music_table = createMusicTable();
+    GHashTable* user_table = createUserTable();
     errosDir();
-    parse_artist("dataset", artist_table);
-    parse_music("dataset", music_table, artist_table);
+    parse_artist("../dataset/artists.csv", artist_table);
+    parse_music("../dataset/musics.csv", music_table, artist_table);
     //parse_user("dataset");
+    freeArtistTable(artist_table);
+    freeMusicTable(music_table);
+    freeUserTable(user_table);
     return 0;
 }
