e1dde1fed738152946e552820e85574d9ae7a7b6
Luis <luisfilipe4ever@gmail.com>
2024-11-05 14:47:19 +0000

    removi comments

--- trabalho-pratico/src/queries.c
+++ trabalho-pratico/src/queries.c
@@ -23,8 +23,6 @@ void query1(char* user_username, GHashTable* user_table, FILE* output_file){
     int age = calculaIdade(getUserBirthDate(user));
 
     fprintf(output_file, "%s;%s;%s;%d;%s\n", getUserEmail(user), getUserFirstName(user), getUserLastName(user), age, getUserCountry(user));
-
-    //freeUser(user);
 }
 
 // Results é necessário para escrever pela ordem pedida
@@ -303,10 +301,6 @@ void artistDurationAdd(G_GNUC_UNUSED gpointer musicId, gpointer musicData, gpoin
     for(int i=0; i<numArtists; i++){
         durationAdd(disco, duration, musicArtistsId[i]);
     }
-    /*
-    free(duration);
-    free(musicArtistsId);
-    */
 }
 
 // Função para ordenar a discografia por durações
